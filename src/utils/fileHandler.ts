d46622be1ec0e3464930ade932de3208965fd1d1
leghair <zweizhao@foxmail.com>
2024-01-28 20:04:55 +0800

    feat 提供text2Voice函数，并测试通过

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,3 +1,6 @@
+import { serverUrl } from '@/utils/config'
+import lc from '@/libs/lc'
+
 export function chooseFile(cb: (files?: FileList) => void, multiple = true, accept?: string) {
   const input = document.createElement('input')
   input.type = 'file'
@@ -22,4 +25,18 @@ export function chooseFile(cb: (files?: FileList) => void, multiple = true, acce
 export function file2BlobUrl(file: File | url) {
   if (typeof file === 'string') return file
   return URL.createObjectURL(file)
+}
+
+export async function text2Voice(text: string, voiceType: number): Promise<{ url: string }> {
+  const ret = await fetch(serverUrl + '/api/tencent/text2voice', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text, voiceType }),
+  }).then(ret => ret.json())
+  // console.log(ret.data.Audio)
+  let audioBase64 = ret.data.Audio
+  const uploadRet = await lc.uploadBase64(audioBase64, Date.now + '.mp3')
+  return { url: uploadRet.get('url') }
 }
