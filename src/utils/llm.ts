1ef09ac80d730e59e867a2bf0d64b9961dd73c4e
leghair <zweizhao@foxmail.com>
2024-02-03 15:32:38 +0800

    feat llm函数使用await

--- src/utils/llm.ts
+++ src/utils/llm.ts
@@ -34,43 +34,52 @@ async function doFetchStream(content: string) {
   return response
 }
 
-function doParseStreamChunk(response: Response, SseCB: LLMCB, doneCB?: LLMCB) {
+async function doParseStreamChunk(response: Response, sseCB: LLMCB, doneCB?: LLMCB) {
   const reader = response.body!.getReader()
+  const decoder = new TextDecoder('utf-8')
+  let value: Uint8Array | undefined
+  let done = false
   let text = ''
   let chunk = ''
-  let json: GLMResponseJSON
-  let temp_text = '' // 临时存储一节无法parse的字符串, 追加到可以parse为止
   let result_text = ''
-  reader.read().then(function process({ done, value }) {
-    text = new TextDecoder('utf-8').decode(value)
-    console.log(text) // 调试用, 有一些奇奇怪怪的问题
-    let textList = text.split('\n\n')
-    for (text of textList) {
-      if (!text.trim()) continue
-      text = (temp_text + text).replace(/data:\s|\[DONE\]/g, '')
-      try {
-        if (text.match(/^{.*}]}$/)) {
-          json = JSON.parse(text) as GLMResponseJSON
-          temp_text = ''
-          chunk = json.choices[0].delta.content
-          if (chunk.trim()) {
-            result_text += chunk
-            // console.log(result_text)
-            SseCB && SseCB(result_text)
+  let temp_text = '' // 临时存储一节无法parse的字符串, 追加到可以parse为止
+  let json: GLMResponseJSON
+  let readerRet: ReadableStreamReadResult<Uint8Array>
+  while (true) {
+    readerRet = await reader.read()
+    value = readerRet.value
+    done = readerRet.done
+    // console.log(value)
+    if (value) {
+      text = decoder.decode(value)
+      // console.log(text)
+      let textList = text.split('\n\n')
+      for (text of textList) {
+        if (!text.trim()) continue
+        text = (temp_text + text).replace(/data:\s|\[DONE\]/g, '')
+        try {
+          if (text.match(/^{.*}]}$/)) {
+            json = JSON.parse(text) as GLMResponseJSON
+            temp_text = ''
+            chunk = json.choices[0].delta.content
+            if (chunk.trim()) {
+              result_text += chunk
+              // console.log(result_text)
+              sseCB && sseCB(result_text)
+            }
+          } else {
+            temp_text += text
           }
-        } else {
-          temp_text += text
+        } catch (error) {
+          console.log(text)
+          console.error(error)
         }
-      } catch (error) {
-        console.log(text)
-        console.error(error)
       }
     }
     if (done) {
       console.log('done!!!')
       doneCB && doneCB(result_text)
-      return
+      break
     }
-    reader.read().then(process)
-  })
+  }
 }
