3f3cbaea7b7774b951e1912b27214251eadaa396
leghair <zweizhao@foxmail.com>
2024-02-03 16:53:25 +0800

    feat 更新astro配置，提供默认地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ai.ilovecats.cn/admin/',
+  base: '/admin/',
+  trailingSlash: "always",
   integrations: [vue(), tailwind()],
+  compressHTML: true,
 })
