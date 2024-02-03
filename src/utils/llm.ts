22d972a9b4d13f5c94dfccde72d319b45a04a398
leghair <zweizhao@foxmail.com>
2024-02-03 15:53:10 +0800

    feat 处理对GLM的字符显示处理优化

--- src/utils/llm.ts
+++ src/utils/llm.ts
@@ -34,16 +34,47 @@ async function doFetchStream(content: string) {
   return response
 }
 
+
+/**
+ * 对 GLM 返回的 Stream 且是 JSON 的字符串处理
+ * @param text 待处理的字符
+ * @param tempText 上次未能 parse 的尾部字符
+ * @returns {jsonList, tempText} 其中的 tempText 是未能parse的字符串, 需要追加到下一次parse
+ */
+function parseGlmStreamChunkJsons2JsonList(text: string, tempText: string): { jsonList: GLMResponseJSON[], tempText: string } {
+  // console.log(text)
+  let json: GLMResponseJSON
+  let jsonList: GLMResponseJSON[] = []
+  let textList = text.split('\n\n')
+  for (text of textList) {
+    if (!text.trim()) continue
+    text = (tempText + text).replace(/data:\s|\[DONE\]/g, '')
+    try {
+      if (text.match(/^{.*}]}$/)) {
+        json = JSON.parse(text) as GLMResponseJSON
+        jsonList.push(json)
+        tempText = ''
+      } else {
+        tempText += text
+      }
+    } catch (error) {
+      // console.log(text)
+      console.error(error)
+    }
+  }
+  return { jsonList, tempText }
+}
+
 async function doParseStreamChunk(response: Response, sseCB: LLMCB, doneCB?: LLMCB) {
   const reader = response.body!.getReader()
   const decoder = new TextDecoder('utf-8')
   let value: Uint8Array | undefined
   let done = false
+  let parseRet: { jsonList: GLMResponseJSON[], tempText: string }
   let text = ''
-  let chunk = ''
-  let result_text = ''
-  let temp_text = '' // 临时存储一节无法parse的字符串, 追加到可以parse为止
-  let json: GLMResponseJSON
+  let content = ''
+  let resultText = ''
+  let tempText = '' // 临时存储一节无法parse的字符串, 追加到可以parse为止
   let readerRet: ReadableStreamReadResult<Uint8Array>
   while (true) {
     readerRet = await reader.read()
@@ -52,33 +83,22 @@ async function doParseStreamChunk(response: Response, sseCB: LLMCB, doneCB?: LLM
     // console.log(value)
     if (value) {
       text = decoder.decode(value)
-      // console.log(text)
-      let textList = text.split('\n\n')
-      for (text of textList) {
-        if (!text.trim()) continue
-        text = (temp_text + text).replace(/data:\s|\[DONE\]/g, '')
-        try {
-          if (text.match(/^{.*}]}$/)) {
-            json = JSON.parse(text) as GLMResponseJSON
-            temp_text = ''
-            chunk = json.choices[0].delta.content
-            if (chunk.trim()) {
-              result_text += chunk
-              // console.log(result_text)
-              sseCB && sseCB(result_text)
-            }
-          } else {
-            temp_text += text
-          }
-        } catch (error) {
-          console.log(text)
-          console.error(error)
+      parseRet = parseGlmStreamChunkJsons2JsonList(text, tempText)
+      tempText = parseRet.tempText
+      for (const json of parseRet.jsonList) {
+        content = json.choices[0].delta.content
+        if (content.trim()) {
+          resultText += content
+          // console.log(resultText)
+          sseCB && sseCB(resultText)
         }
+        // 使用await 延迟10ms
+        await new Promise(resolve => setTimeout(resolve, 50))
       }
     }
     if (done) {
       console.log('done!!!')
-      doneCB && doneCB(result_text)
+      doneCB && doneCB(resultText)
       break
     }
   }
