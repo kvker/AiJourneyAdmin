52ab48793c737e68192360754e849a2c38fd633c
leghair <zweizhao@foxmail.com>
2024-02-21 20:02:16 +0800

    feat 优化打包分包

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,34 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // chunkFileNames: 'static/js/[name]-[hash].js',
+        // entryFileNames: 'static/js/[name]-[hash].js',
+        // assetFileNames: 'static/[ext]/[name]-[hash][extname]',
+        //manualChunks 两种使用形式
+        manualChunks: {
+          leancloudStorage: ['leancloud-storage'],
+          // daisyUI: ['daisyUI'],
+          // daisyui: ['daisyui'],
+          vue: ['vue'],
+          // vueRouter: ['vue-router'],
+          echarts: ['echarts'],
+        }
+        // manualChunks(id) {
+        //   if (id.includes('daisyUI')) {
+        //     return 'daisyUI'
+        //   }
+        // }
+        // manualChunks(id) {
+        //   // id为文件的绝对路径
+        //   if (id.includes('node_modules')) {
+        //     return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        //   }
+        // }
+      }
+    }
   }
 })
