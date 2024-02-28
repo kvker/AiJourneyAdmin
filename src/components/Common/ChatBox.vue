8511e9672ad8b4abf34e07c8593ef8b4c09e19e3
leghair <zweizhao@foxmail.com>
2024-02-28 15:42:06 +0800

    feat 优化对话

--- src/components/Common/ChatBox.vue
+++ src/components/Common/ChatBox.vue
@@ -12,33 +12,30 @@ const onToggleExpand = () => chatBoxShow.value = !chatBoxShow.value
 const lastContent = ref('')
 const textarea: Ref<HTMLTextAreaElement | undefined> = ref()
 
-type Chat = {
-  content: string
-  role: 'user' | 'assitant'
-}
-type ChatList = Chat[]
+type ChatList = GLMMessage[]
 
 const chatList: Ref<ChatList> = ref([])
 
-const onCreateChat = (content: string, role: 'user' | 'assitant') => {
+const onCreateChat = (content: string, role: GLMMessage["role"]) => {
   chatList.value.push({ content, role })
 }
 
 const onChat = () => {
   const lastChat = chatList.value.at(-1)
   if (lastChat) {
-    onCompletions(lastChat.content, (result) => {
+    onCompletions(chatList.value, (result) => {
       lastContent.value = result
     }, done => {
       if (done) {
-        onCreateChat(lastContent.value, 'assitant')
+        onCreateChat(lastContent.value, 'assistant')
         lastContent.value = ''
       }
     })
   }
 }
 
-const onSend = () => {
+const onSend = (e: KeyboardEvent | MouseEvent) => {
+  e.preventDefault()
   const content = textarea.value!.value.trim()
   if (content) {
     onCleanTextarea()
@@ -54,7 +51,8 @@ const onCleanTextarea = () => {
 
 <template>
   <div class=" fixed bottom-20 right-20">
-    <div class="fab w-24 h-24 rounded-full border-2 font-bold text-4xl flex justify-center items-center cursor-pointer shadow-2xl"
+    <div
+      class="fab w-24 h-24 rounded-full border-2 font-bold text-4xl flex justify-center items-center cursor-pointer shadow-2xl bg-white"
       v-show="!chatBoxShow" @click="onToggleExpand">展开</div>
     <div class="chat-box bg-white shadow-2xl p-4" v-show="chatBoxShow">
       <div class="chat-list-box w-full">
@@ -62,8 +60,8 @@ const onCleanTextarea = () => {
           <div class="chat-bubble chat-bubble-accent">你好啊，有什么需要帮助的么？</div>
         </div>
         <div v-for="(chat, index) of chatList" :key="`chat${index}`" class="chat chat-start"
-          :class="chat.role === 'assitant' ? 'chat-start' : 'chat-end'">
-          <div class="chat-bubble" :class="chat.role === 'assitant' ? 'chat-bubble-accent' : 'chat-bubble-info'">{{
+          :class="chat.role === 'assistant' ? 'chat-start' : 'chat-end'">
+          <div class="chat-bubble" :class="chat.role === 'assistant' ? 'chat-bubble-accent' : 'chat-bubble-info'">{{
             chat.content }}</div>
         </div>
         <div v-if="lastContent" class="chat chat-start">
@@ -71,7 +69,8 @@ const onCleanTextarea = () => {
         </div>
       </div>
       <div class="chat-input-box flex justify-between items-center">
-        <textarea class=" flex-1 h-32 border-none outline-none p-2 shadow-md" placeholder="请描述您的需求" ref="textarea" @keyup.shift.enter.prevent="onSend"></textarea>
+        <textarea class=" flex-1 h-32 border-none outline-none p-2 shadow-md" placeholder="请描述您的需求" ref="textarea"
+          @keydown.shift.enter="onSend"></textarea>
         <div class=" flex flex-col">
           <button class=" h-12 text-red-400" @click="onSend">发送</button>
           <button class=" h-12 text-grey-400" @click="onToggleExpand">关闭</button>
