eec905130ff614ccebe002f39bf05afaf4b67f9b
leghair <zweizhao@foxmail.com>
2024-03-06 12:35:46 +0800

    feat 前端拦截一下非管理员进入

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import { auth, db } from '@/services/cloud'
 
 // , {
 //   component: Shop,
@@ -34,6 +34,21 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView,
+      beforeEnter: async (to, from, next) => {
+        const { data } = await db.collection('Role').where({
+          userIds: auth.currentUser?.uid
+        })
+          .get()
+        console.log('用户角色： ' + data[0]?.name)
+        const passed = data[0]?.name === 'superAdmin'
+        if (passed) {
+          next()
+        } else {
+          alert('抱歉，您没有权限进入管理系统')
+          await auth.signOut()
+          next('/')
+        }
+      },
       children: [
         {
           path: '',
