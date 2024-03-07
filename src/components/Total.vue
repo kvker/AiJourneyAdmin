a99a8fe017879f9aca6baf8e5735aeb1303a15c1
leghair <zweizhao@foxmail.com>
2024-03-07 16:54:39 +0800

    feat 优化首页自动标记选中的景区名

--- src/components/Total.vue
+++ src/components/Total.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import type { Ref } from 'vue'
 import { db } from '@/services/cloud'
 
@@ -8,25 +8,32 @@ const attractionList: Ref<Attraction[]> = ref([])
 const getAttractionList = async () => {
   const { data } = await db.collection('JAttraction').get()
   attractionList.value = data
-  attraction.value = data[0]
+  if (!attraction.value) {
+    attraction.value = data[0]
+  }
   localStorage.setItem('attraction', JSON.stringify(attraction.value))
 }
 getAttractionList()
 
+const attractionName = computed(() => {
+  return attractionList.value.find((item) => item.name === attraction.value?.name)?.name
+})
+
 const onChangeAttraction = (e: Event) => {
   const select = e.target as HTMLSelectElement
-  const index = select.value
-  attraction.value = attractionList.value[Number(index)]
+  const name = select.value
+  attraction.value = attractionList.value.find((item) => item.name === name) as Attraction
   localStorage.setItem('attraction', JSON.stringify(attraction.value))
 }
 </script>
 
 <template>
   <div class="w-full h-full">
-    <select class="select select-info w-full max-w-xs" @change="onChangeAttraction">
+    <select class="select select-info w-full max-w-xs" @change="onChangeAttraction" :value="attractionName">
       <option disabled selected>选择景区</option>
-      <option v-for="(attraction, index) of attractionList" :key="attraction._id" :value="index">{{ attraction.name
-        }}
+      <option v-for="(attraction, index) of attractionList" :key="attraction._id" :value="attraction.name">{{
+      attraction.name
+    }}
       </option>
     </select>
     <p>
