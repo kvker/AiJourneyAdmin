36f5ab77ad489de66c937f5353931ada2d0cb457
leghair <zweizhao@foxmail.com>
2024-03-19 00:21:57 +0800

    feat 添加一键新增工具

---
+++ scripts/text2json.js
@@ -0,0 +1,36 @@
+/**
+ * 将复制出来的EXCEL数据转成JSON数组
+ */
+
+const fs = require('fs')
+
+fs.readFile('1.txt', 'utf8', (err, data) => {
+  if(err) console.error(err)
+  else {
+    const arr = data.split('\n')
+    console.log(arr.length)
+    const json = []
+    let obj = {}
+    arr.forEach(item => {
+      // console.log(item)
+      const [name, introduce, lnglat] = item.split('\t')
+      if(!name) return
+      const [longitude, latitude] = lnglat.replace('经度:', '').replace('纬度:', '').split(' ')
+      obj = {
+        name,
+        innerName: name,
+        introduce,
+        lnglat: {
+          longitude,
+          latitude
+        },
+      }
+      json.push(obj)
+    })
+    console.log(json)
+    fs.writeFile('1.json', JSON.stringify(json), (err) => {
+      if(err) console.error(err)
+      else console.log('写入成功')
+    })
+  }
+})
--- src/components/Area/Query.vue
+++ src/components/Area/Query.vue
@@ -1,12 +1,29 @@
 <script lang="ts" setup>
 import { inject } from 'vue'
 import type { Ref } from 'vue'
+import { db } from '@/services/cloud'
+
 const searchParams = inject('searchParams') as Ref<AreaSearchParams>
 
 const emit = defineEmits(['add'])
 
 function onAdd() {
   emit('add')
+}
+
+async function onMutipleAdd() {
+  const json = JSON.parse(prompt('粘贴JSON数据') as string)
+  if (json.length) {
+    const attraction = JSON.parse(localStorage.getItem('attraction') as string)
+    for (const item of json) {
+      await db.collection('JArea').add({
+        ...item,
+        attractionId: attraction._id,
+        coverImageList: [],
+        updatedAt: new Date(),
+      })
+    }
+  }
 }
 </script>
 
@@ -18,6 +35,7 @@ function onAdd() {
     </label>
     <label class=" ml-2">
       <button class="btn btn-primary" type="submit">新增</button>
+      <div class="btn btn-primary ml-4" @click="onMutipleAdd">一键新增（JSON）</div>
     </label>
   </form>
 </template>
